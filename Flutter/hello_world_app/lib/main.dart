e3ed8afd2e9e8502da888c43de6de8ed364c4f6d
Brian Coyago <briancoyago@hotmail.com>
2025-11-29 20:36:36 -0500

    feat(Flutter hello_world_app): AppBar and Material Design 3
    Brian Coyago

--- Flutter/hello_world_app/lib/main.dart
+++ Flutter/hello_world_app/lib/main.dart
@@ -6,15 +6,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       // Remove debug tag
       debugShowCheckedModeBanner: false,
-      home: CounterScreen()
+      theme: ThemeData(colorSchemeSeed: Colors.blue),
+      home: const CounterScreen(),
     );
   }
 }
