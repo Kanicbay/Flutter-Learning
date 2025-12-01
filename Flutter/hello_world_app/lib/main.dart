b95da876a78c80bc78572e3da2946b3475446ab5
Brian Coyago <briancoyago@hotmail.com>
2025-11-30 20:39:14 -0500

    feat(Flutter hello_world_app): use ternary operator for clicks
    Brian Coyago

--- Flutter/hello_world_app/lib/main.dart
+++ Flutter/hello_world_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hello_world_app/presentation/screens/counter_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
