e365da6848c20b6e87201fe3cda3db177b428a79
Brian Coyago <briancoyago@hotmail.com>
2025-12-01 20:42:35 -0500

    feat(Flutter yes_no_app): Use circle avatar for chatting and list view
    Brian Coyago

--- Flutter/yes_no_app/lib/presentation/chat/chat_screen.dart
+++ Flutter/yes_no_app/lib/presentation/chat/chat_screen.dart
@@ -7,7 +7,7 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Mi amor ♥'),
+        title: const Text('Mi amor (私の愛 ♥)'),
         leading: const Padding(
           padding: EdgeInsets.all(8.0),
           child: CircleAvatar(
@@ -15,6 +15,31 @@ class ChatScreen extends StatelessWidget {
               'https://pbs.twimg.com/media/Gqq9OOMbgAAft6C.jpg',
             ),
           ),
+        ),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Text('Indice $index');
+                },
+              ),
+            ),
+            const Text('Mundo'),
+          ],
         ),
       ),
     );
