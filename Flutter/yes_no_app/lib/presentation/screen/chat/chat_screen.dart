42c6ff6bf1ba4330fd7fe57a450e0bf7e9bc7157
Brian Coyago <briancoyago@hotmail.com>
2025-12-04 16:18:01 -0500

    fix(Flutter yes_no_app): remove actions
    Brian Coyago

--- Flutter/yes_no_app/lib/presentation/screen/chat/chat_screen.dart
+++ Flutter/yes_no_app/lib/presentation/screen/chat/chat_screen.dart
@@ -35,9 +35,6 @@ class ChatScreen extends StatelessWidget {
             ),
           ),
         ),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
-        ],
       ),
       body: _ChatView(),
     );
