690f7886cc0bfac8b6300410c7e1e27c87773adf
Brian Coyago <briancoyago@hotmail.com>
2025-12-04 13:56:47 -0500

    feat(Flutter yes_no_app): change stateless widget to stateful on MessageFieldBox
    Brian Coyago

--- Flutter/yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
+++ Flutter/yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 
-class MessageFieldBox extends StatelessWidget {
+class MessageFieldBox extends StatefulWidget {
   final ValueChanged<String> onValue;
 
   const MessageFieldBox({super.key, required this.onValue});
 
   @override
-  Widget build(BuildContext context) {
-    final textController = TextEditingController();
-    final focusNode = FocusNode();
+  State<MessageFieldBox> createState() => _MessageFieldBoxState();
+}
 
+class _MessageFieldBoxState extends State<MessageFieldBox> {
+  late TextEditingController textController;
+  late FocusNode focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+    focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final outlineInputBorder = UnderlineInputBorder(
       borderSide: const BorderSide(color: Colors.transparent),
       borderRadius: BorderRadius.circular(40),
@@ -19,12 +38,13 @@ class MessageFieldBox extends StatelessWidget {
       hintText: 'End your message with a "?"',
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
+      contentPadding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
       filled: true,
       suffixIcon: IconButton(
         onPressed: () {
           final textValue = textController.value.text;
           textController.clear();
-          onValue(textValue);
+          widget.onValue(textValue);
         },
         icon: const Icon(Icons.send_outlined),
       ),
@@ -40,7 +60,7 @@ class MessageFieldBox extends StatelessWidget {
       onFieldSubmitted: (value) {
         textController.clear();
         focusNode.requestFocus();
-        onValue(value);
+        widget.onValue(value);
       },
     );
   }
