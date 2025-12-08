a03ea6c22f7eec092c45f0151436f60374b36d14
Brian Coyago <briancoyago@hotmail.com>
2025-12-07 20:30:10 -0500

    feat(toktik): create material app and add dark mode
    Brian Coyago

---
+++ Flutter/toktik/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/config/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Toktik',
+      home: Scaffold(
+        appBar: AppBar(title: const Center(child: Text('Material App Bar'))),
+
+        body: const Center(child: Text('Hello World')),
+      ),
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+    );
+  }
+}
