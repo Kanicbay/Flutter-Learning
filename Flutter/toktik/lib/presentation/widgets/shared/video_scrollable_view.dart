8c0b9fb4d1d1a618926af658d44e11858deeaff9
Brian Coyago <briancoyago@hotmail.com>
2025-12-10 18:30:12 -0500

    feat(toktik): add video buttons widget and custom button
    Brian Coyago

--- Flutter/toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ Flutter/toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -8,17 +9,28 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       physics: const BouncingScrollPhysics(),
       scrollDirection: Axis.vertical,
-      children: [
-        Container(color: Colors.red),
-        Container(color: Colors.blue),
-        Container(color: Colors.green),
-        Container(color: Colors.teal),
-        Container(color: Colors.yellow),
-        Container(color: Colors.deepPurple),
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          children: [
+            // Video Player + gradiente
+
+            // Botones
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost),
+            ),
+          ],
+        );
+      },
     );
   }
 }
+
+
