44560b21611e2e2109b06911705796d46271953c
Brian Coyago <briancoyago@hotmail.com>
2025-12-10 23:23:04 -0500

    feat(toktik): add animations by animation do
    animation do is a library made by fernando herrera
    
    Brian Coyago

--- Flutter/toktik/lib/presentation/widgets/shared/video_buttons.dart
+++ Flutter/toktik/lib/presentation/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
@@ -16,9 +17,19 @@ class VideoButtons extends StatelessWidget {
           iconData: Icons.favorite,
           iconColor: Colors.red,
         ),
+        const SizedBox(height: 20),
         _CustomIconButton(
           value: video.views,
           iconData: Icons.remove_red_eye_outlined,
+        ),
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: const _CustomIconButton(
+            value: 0,
+            iconData: Icons.play_circle_outlined,
+          ),
         ),
       ],
     );
@@ -44,7 +55,7 @@ class _CustomIconButton extends StatelessWidget {
           onPressed: () {},
           icon: Icon(iconData, color: color, size: 30),
         ),
-        Text(HumanFormats.humanReadbleNumer(value.toDouble())),
+        if (value > 0) Text(HumanFormats.humanReadbleNumer(value.toDouble())),
       ],
     );
   }
