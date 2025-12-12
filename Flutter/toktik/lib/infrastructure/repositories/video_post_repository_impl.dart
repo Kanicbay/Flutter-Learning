568aad29afa9e64a2bc4f5eb2bc369373e05bf75
Brian Coyago <briancoyago@hotmail.com>
2025-12-11 21:54:50 -0500

    feat(toktik): add clean architecture with repositories and datasources
    Brian Coyago

---
+++ Flutter/toktik/lib/infrastructure/repositories/video_post_repository_impl.dart
@@ -0,0 +1,23 @@
+import 'package:toktik/domain/datasources/video_posts_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
+
+class VideoPostRepository implements VideoPostsRepository{
+
+  final VideoPostsDatasource videosDatasource;
+
+  VideoPostRepository({required this.videosDatasource});
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videosDatasource.getTrendingVideosByPage(page);
+  }
+  
+
+
+}
