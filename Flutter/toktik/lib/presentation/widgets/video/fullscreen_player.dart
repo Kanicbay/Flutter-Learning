64c081594d37fd9ee147bb71c7734c6af32b2fc3
Brian Coyago <briancoyago@hotmail.com>
2025-12-11 20:56:55 -0500

    feat(toktik): add gesture detector for pause/play video
    Brian Coyago

--- Flutter/toktik/lib/presentation/widgets/video/fullscreen_player.dart
+++ Flutter/toktik/lib/presentation/widgets/video/fullscreen_player.dart
@@ -44,21 +44,30 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
           return const Center(child: CircularProgressIndicator(strokeWidth: 2));
         }
 
-        return AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: Stack(
-            children: [
-              VideoPlayer(controller),
-
-              // Gradiente
-
-              //Texto
-              Positioned(
-                bottom: 50,
-                left: 20,
-                child: _VideoCaption(caption: widget.caption),
-              ),
-            ],
+        return GestureDetector(
+          onTap: () {
+            if (controller.value.isPlaying) {
+              controller.pause();
+              return;
+            }
+            controller.play();
+          },
+          child: AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(
+              children: [
+                VideoPlayer(controller),
+          
+                // Gradiente
+          
+                //Texto
+                Positioned(
+                  bottom: 50,
+                  left: 20,
+                  child: _VideoCaption(caption: widget.caption),
+                ),
+              ],
+            ),
           ),
         );
       },
