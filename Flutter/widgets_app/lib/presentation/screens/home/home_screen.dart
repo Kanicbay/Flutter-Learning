8035a7cec2ab49abef769eec0268fb39355bc880
Brian Coyago <briancoyago@hotmail.com>
2025-12-13 19:40:37 -0500

    feat(widgets_app): add go router, app router and screens
    Brian Coyago

--- Flutter/widgets_app/lib/presentation/screens/home/home_screen.dart
+++ Flutter/widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -47,7 +47,8 @@ class _CustomListTile extends StatelessWidget {
         // Navigator.of(
         //   context,
         // ).push(MaterialPageRoute(builder: (context) => ButtonsScreen()));
-        Navigator.pushNamed(context, menuItem.link);
+        // Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
