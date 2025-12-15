8f018a0639e5e5897be37c6db2efd9254a5119b9
Brian Coyago <briancoyago@hotmail.com>
2025-12-14 22:55:11 -0500

    feat(widgets_app): create card with border
    Brian Coyago

--- Flutter/widgets_app/lib/presentation/screens/cards/cards_screen.dart
+++ Flutter/widgets_app/lib/presentation/screens/cards/cards_screen.dart
@@ -35,7 +35,10 @@ class _CardsView extends StatelessWidget {
             (card) =>
                 _CardType1(label: card['label'], elevation: card['elevation']),
           ),
-       
+          ...cards.map(
+            (card) =>
+                _CardType2(label: card['label'], elevation: card['elevation']),
+          ),
         ],
       ),
     );
@@ -64,6 +67,44 @@ class _CardType1 extends StatelessWidget {
               ),
             ),
             Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        side: BorderSide(color: colors.outline),
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
+            ),
           ],
         ),
       ),
