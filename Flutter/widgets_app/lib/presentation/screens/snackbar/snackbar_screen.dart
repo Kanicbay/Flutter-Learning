e9c3c8c43633f96243b587cf9c1ffa0a6b42e65a
Brian Coyago <briancoyago@hotmail.com>
2025-12-15 22:20:44 -0500

    feat(widgets_app): add dialogs and alert dialogs
    Brian Coyago

--- Flutter/widgets_app/lib/presentation/screens/snackbar/snackbar_screen.dart
+++ Flutter/widgets_app/lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -17,10 +18,59 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('¿Estás Seguro?'),
+        content: Text(
+          'Parrafor parrafo parrafo parrafo parrafo parrafo parrafo parrafoParrafor parrafo parrafo parrafo parrafo parrafo parrafo parrafoParrafor parrafo parrafo parrafo parrafo parrafo parrafo parrafoParrafor parrafo parrafo parrafo parrafo parrafo parrafo parrafoParrafor parrafo parrafo parrafo parrafo parrafo parrafo parrafoParrafor parrafo parrafo parrafo parrafo parrafo parrafo parrafo',
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(),
+            child: const Text('Cancelar'),
+          ),
+          FilledButton(
+            onPressed: () => context.pop(),
+            child: const Text('Aceptar'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Snackbars y Diálogos')),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text(
+                      'Parrafor parrafo parrafo parrafo parrafo parrafo parrafo parrafoParrafor parrafo parrafo parrafo parrafo parrafo parrafo parrafoParrafor parrafo parrafo parrafo parrafo parrafo parrafo parrafoParrafor parrafo parrafo parrafo parrafo parrafo parrafo parrafoParrafor parrafo parrafo parrafo parrafo parrafo parrafo parrafoParrafor parrafo parrafo parrafo parrafo parrafo parrafo parrafo',
+                    ),
+                  ],
+                );
+              },
+              child: Text('Licencias usadas'),
+            ),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: Text('Mostrar diállogo'),
+            ),
+          ],
+        ),
+      ),
+
       floatingActionButton: FloatingActionButton.extended(
         label: Text('Mostrar Snackbar'),
         icon: Icon(Icons.remove_red_eye_outlined),
