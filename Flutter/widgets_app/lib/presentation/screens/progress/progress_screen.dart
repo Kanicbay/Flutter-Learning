b7c435e4e666dfdaf6ae532ea967682240307dba
Brian Coyago <briancoyago@hotmail.com>
2025-12-15 21:32:25 -0500

    feat(widgets_app): create basic circular progress indicator
    also add controlled circular progress indicator widgety
    
    Brian Coyago

--- Flutter/widgets_app/lib/presentation/screens/progress/progress_screen.dart
+++ Flutter/widgets_app/lib/presentation/screens/progress/progress_screen.dart
@@ -1,14 +1,49 @@
-
 import 'package:flutter/material.dart';
 
 class ProgressScreen extends StatelessWidget {
-
   static const name = 'progress_screen';
 
   const ProgressScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Progress Indicators')),
+      body: _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          const SizedBox(height: 30),
+          Text('Circular Progress Indicator'),
+          const SizedBox(height: 10),
+          CircularProgressIndicator(
+            strokeWidth: 2,
+            backgroundColor: Colors.black45,
+          ),
+          const SizedBox(height: 20),
+          Text('Circular Progress Indicator Controllated'),
+          SizedBox(height: 10),
+          _ControlledProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
