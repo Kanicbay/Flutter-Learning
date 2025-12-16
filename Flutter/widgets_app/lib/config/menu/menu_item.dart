f050862c3653403af84bf2dfc95bb5bbd7216935
Brian Coyago <briancoyago@hotmail.com>
2025-12-15 22:28:17 -0500

    feat(widgets_app): created animated container
    Brian Coyago

--- Flutter/widgets_app/lib/config/menu/menu_item.dart
+++ Flutter/widgets_app/lib/config/menu/menu_item.dart
@@ -41,4 +41,10 @@ final appMenuItems = <MenuItem>[
     link: '/snackbars',
     icon: Icons.info_outline,
   ),
+  MenuItem(
+    title: 'Animated container',
+    subTitle: 'Stateful widget animado',
+    link: '/animated',
+    icon: Icons.check_box_outline_blank_rounded,
+  ),
 ];
--- Flutter/widgets_app/lib/presentation/screens/animated/animated_screen.dart
+++ Flutter/widgets_app/lib/presentation/screens/animated/animated_screen.dart
@@ -1,14 +1,32 @@
-
 import 'package:flutter/material.dart';
 
 class AnimatedScreen extends StatelessWidget {
-
   static const name = 'animated_screen';
 
   const AnimatedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Animated Container')),
+
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.easeInCubic,
+          width: 300,
+          height: 230,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.play_arrow_rounded),
+      ),
+    );
   }
 }
