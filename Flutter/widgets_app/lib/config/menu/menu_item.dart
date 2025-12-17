753efb75c72dcc88c9e7cc33864b7757fb05f454
Brian Coyago <briancoyago@hotmail.com>
2025-12-17 16:08:33 -0500

    feat(widgets_app): create infinite scroll screen
    add also remove padding with mediaquery
    
    Brian Coyago

--- Flutter/widgets_app/lib/config/menu/menu_item.dart
+++ Flutter/widgets_app/lib/config/menu/menu_item.dart
@@ -59,4 +59,10 @@ final appMenuItems = <MenuItem>[
     link: '/tutorial',
     icon: Icons.accessibility_rounded,
   ),
+  MenuItem(
+    title: 'InfiniteScroll y Pull',
+    subTitle: 'Listas infinitas y pull to refresh',
+    link: '/infinite',
+    icon: Icons.list_alt_rounded,
+  ),
 ];
