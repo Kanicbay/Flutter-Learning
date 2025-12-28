aa0c676b19e499b8d7c6e8a8b0e134f8c88dd6e7
Brian Coyago <briancoyago@hotmail.com>
2025-12-28 12:52:51 -0500

    feat(cinemapedia): use mapper into getNowPlaying
    Brian Coyago

--- Flutter/cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
+++ Flutter/cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
@@ -9,17 +11,21 @@ class MoviedbDatasource extends MoviesDatasource {
       baseUrl: 'https://api.themoviedb.org/3',
       queryParameters: {
         'api_key': Environment.theMovieDBKey,
-        'language': 'es-MX'
-      }
-    )
+        'language': 'es-MX',
+      },
+    ),
   );
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
-     
+    final moviedDBResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = moviedDBResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDBtoEntity(moviedb))
+        .toList();
+
     return movies;
   }
 }
--- Flutter/cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
+++ Flutter/cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
@@ -15,7 +15,7 @@ class MovieMapper {
     popularity: moviedb.popularity,
     posterPath: (moviedb.posterPath != '')
         ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-        : 'https://cdn.displate.com/artwork/270x380/2024-04-10/5def7f369411aed6369a287a35674726_ff36b6b0233da0913042c802de7948e5.jpg',
+        : 'no-poster',
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
     video: moviedb.video,
