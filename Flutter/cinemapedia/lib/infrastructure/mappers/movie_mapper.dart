12b383e4bbd310b54d4881de3bf621b4d9c7ef04
Brian Coyago <briancoyago@hotmail.com>
2025-12-27 20:29:41 -0500

    feat(cinemapedia): create mapper for movieDb
    Brian Coyago

---
+++ Flutter/cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBtoEntity(MovieMovieDB moviedb) => Movie(
+    adult: moviedb.adult,
+    backdropPath: moviedb.backdropPath != ''
+        ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+        : 'https://cdn.displate.com/artwork/270x380/2024-04-10/5def7f369411aed6369a287a35674726_ff36b6b0233da0913042c802de7948e5.jpg',
+    genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+    id: moviedb.id,
+    originalLanguage: moviedb.originalLanguage,
+    originalTitle: moviedb.originalTitle,
+    overview: moviedb.overview,
+    popularity: moviedb.popularity,
+    posterPath: (moviedb.posterPath != '')
+        ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+        : 'https://cdn.displate.com/artwork/270x380/2024-04-10/5def7f369411aed6369a287a35674726_ff36b6b0233da0913042c802de7948e5.jpg',
+    releaseDate: moviedb.releaseDate,
+    title: moviedb.title,
+    video: moviedb.video,
+    voteAverage: moviedb.voteAverage,
+    voteCount: moviedb.voteCount,
+  );
+}
