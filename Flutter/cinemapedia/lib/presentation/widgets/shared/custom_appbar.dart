435910acb58839fd5885e3870b17a65b6154e4d4
Brian Coyago <briancoyago@hotmail.com>
2025-12-29 15:56:22 -0500

    chore(cinemapedia): remove botton padding appbar
    Brian Coyago

--- Flutter/cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
+++ Flutter/cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
@@ -9,6 +9,7 @@ class CustomAppbar extends StatelessWidget {
     final titleStyle = Theme.of(context).textTheme.titleMedium;
 
     return SafeArea(
+      bottom: false,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: SizedBox(
