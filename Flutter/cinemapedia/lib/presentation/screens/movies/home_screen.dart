685d3ca973237009a3b17cf1c84265a84b5b9c71
Brian Coyago <briancoyago@hotmail.com>
2025-12-29 16:29:39 -0500

    feat(cinemapedia): reduce slide show movies with provider
    Brian Coyago

--- Flutter/cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ Flutter/cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -30,12 +30,14 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    // final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final slideShowMovies = ref.watch(moviesSlideshowProvider);
 
     return Column(
       children: [
         const CustomAppbar(),
-        MoviesSlideshow(movies: nowPlayingMovies),
+
+        MoviesSlideshow(movies: slideShowMovies),
       ],
     );
   }
