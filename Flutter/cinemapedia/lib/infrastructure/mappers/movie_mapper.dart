d5588e9b0dc68408193d5aacb80116d427da179c
Brian Coyago <briancoyago@hotmail.com>
2026-01-03 13:21:31 -0500

    feat(cinemapedia): show title and overview movie
    Brian Coyago

--- Flutter/cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
+++ Flutter/cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
@@ -18,7 +18,7 @@ class MovieMapper {
         : moviedb.popularity.toInt().toDouble(),
     posterPath: (moviedb.posterPath != '')
         ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-        : 'no-poster',
+        : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s',
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
     video: moviedb.video,
@@ -26,7 +26,8 @@ class MovieMapper {
     voteCount: moviedb.voteCount,
   );
 
-  static Movie movieDetailsToEntity(MovieDetails moviedb) => Movie(adult: moviedb.adult,
+  static Movie movieDetailsToEntity(MovieDetails moviedb) => Movie(
+    adult: moviedb.adult,
     backdropPath: moviedb.backdropPath != ''
         ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
         : 'https://cdn.displate.com/artwork/270x380/2024-04-10/5def7f369411aed6369a287a35674726_ff36b6b0233da0913042c802de7948e5.jpg',
@@ -45,5 +46,6 @@ class MovieMapper {
     title: moviedb.title,
     video: moviedb.video,
     voteAverage: moviedb.voteAverage,
-    voteCount: moviedb.voteCount,);
+    voteCount: moviedb.voteCount,
+  );
 }
