3840b603c4b050f6dcba7496c9012825b72de467
Brian Coyago <briancoyago@hotmail.com>
2026-01-06 16:32:03 -0500

    feat(cinemapedia): show first favorite movies
    Brian Coyago

--- Flutter/cinemapedia/lib/presentation/views/home_views/favorites_view.dart
+++ Flutter/cinemapedia/lib/presentation/views/home_views/favorites_view.dart
@@ -1,16 +1,32 @@
+import 'package:cinemapedia/presentation/providers/storage/favorite_movies_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  ConsumerState<FavoritesView> createState() => _FavoritesViewState();
+}
+
+class _FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    
+    final favoriteMovies = ref.watch(favoriteMoviesProvider);
+    final movies = favoriteMovies.values.toList();
+
     return Scaffold(
       body: ListView.builder(
-        itemCount: 10,
+        itemCount: favoriteMovies.keys.length,
         itemBuilder: (context, index) {
-          return ListTile(title: Text('Movie name'));
+          [index];
+          return ListTile(title: Text(movies[index].title));
         },
       ),
     );
