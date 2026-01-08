a8d55af53b677a485757cde983e5b8681a820d7c
Brian Coyago <briancoyago@hotmail.com>
2026-01-07 20:01:06 -0500

    feat(cinemapedia): add action to movies slideshow
    Brian Coyago

--- Flutter/cinemapedia/lib/presentation/widgets/movies/movies_slideshow.dart
+++ Flutter/cinemapedia/lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,7 +1,7 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MoviesSlideshow extends StatelessWidget {
   final List<Movie> movies;
@@ -43,11 +43,7 @@ class _Slide extends StatelessWidget {
     final decoration = BoxDecoration(
       borderRadius: BorderRadius.circular(20),
       boxShadow: const [
-        BoxShadow(
-          color: Colors.black45,
-          blurRadius: 10,
-          offset: Offset(0, 10),
-        ),
+        BoxShadow(color: Colors.black45, blurRadius: 10, offset: Offset(0, 10)),
       ],
     );
 
@@ -57,17 +53,13 @@ class _Slide extends StatelessWidget {
         decoration: decoration,
         child: ClipRRect(
           borderRadius: BorderRadiusGeometry.circular(20),
-          child: Image.network(
-            movie.backdropPath,
-            fit: BoxFit.cover,
-            loadingBuilder: (context, child, loadingProgress) {
-              if (loadingProgress != null) {
-                return const DecoratedBox(
-                  decoration: BoxDecoration(color: Colors.black12),
-                );
-              }
-              return FadeIn(child: child);
-            },
+          child: GestureDetector(
+            onTap: () => context.push('/movie/${movie.id}'),
+            child: FadeInImage(
+              fit: BoxFit.cover,
+              placeholder: const AssetImage('assets/loaders/bottle-loader.gif'),
+              image: NetworkImage(movie.backdropPath),
+            ),
           ),
         ),
       ),
