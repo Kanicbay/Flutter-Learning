d50e3f0f2dd672010c118a4109fe99fc9aeaffe3
Brian Coyago <briancoyago@hotmail.com>
2026-01-08 19:19:21 -0500

    feat(BloC): reduce logic into counter bloc
    Brian Coyago

--- Flutter/forms_app/lib/presentation/blocs/counter_bloc/counter_bloc.dart
+++ Flutter/forms_app/lib/presentation/blocs/counter_bloc/counter_bloc.dart
@@ -6,13 +6,14 @@ part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(const CounterState()) {
-    on<CounterIncreased>((event, emit) {
-      emit(
-        state.copyWith(
-          counter: state.counter + event.value,
-          transactionCount: state.transactionCount + 1,
-        ),
-      );
-    });
+    on<CounterIncreased>(_onCounterIncresead);
+  }
+  void _onCounterIncresead(CounterIncreased event, Emitter<CounterState> emit) {
+    emit(
+      state.copyWith(
+        counter: state.counter + event.value,
+        transactionCount: state.transactionCount + 1,
+      ),
+    );
   }
 }
