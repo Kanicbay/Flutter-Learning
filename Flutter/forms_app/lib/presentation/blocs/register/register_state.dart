a7dfc770533d6eb606043956195ced75f38598e8
Brian Coyago <briancoyago@hotmail.com>
2026-01-09 17:07:46 -0500

    feat(forms): create username input
    Brian Coyago

--- Flutter/forms_app/lib/presentation/blocs/register/register_state.dart
+++ Flutter/forms_app/lib/presentation/blocs/register/register_state.dart
@@ -4,29 +4,32 @@ enum FormStatus { invalid, valid, validating, posting }
 
 class RegisterFormState extends Equatable {
   final FormStatus formStatus;
-  final String username;
+  final bool isValid;
+  final Username username;
   final String email;
   final String password;
 
   const RegisterFormState({
     this.formStatus = FormStatus.invalid,
-    this.username = '',
+    this.username = const Username.pure(),
     this.email = '',
-    this.password = '',
+    this.password = '', this.isValid = false,
   });
 
   RegisterFormState copyWith({
     FormStatus? formStatus,
-    String? username,
+    Username? username,
+    bool? isValid,
     String? email,
     String? password,
   }) => RegisterFormState(
     formStatus: formStatus ?? this.formStatus,
     username: username ?? this.username,
+    isValid: isValid ?? this.isValid,
     email: email ?? this.email,
     password: password ?? this.password,
   );
 
   @override
-  List<Object> get props => [formStatus, username, email, password];
+  List<Object> get props => [formStatus, isValid, username, email, password];
 }
