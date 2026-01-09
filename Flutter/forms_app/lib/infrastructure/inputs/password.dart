106acdbd335b09809dab0d0d5dd1c82088bd9dea
Brian Coyago <briancoyago@hotmail.com>
2026-01-09 17:32:11 -0500

    feat(forms): implements password input and message error
    Brian Coyago

--- Flutter/forms_app/lib/infrastructure/inputs/password.dart
+++ Flutter/forms_app/lib/infrastructure/inputs/password.dart
@@ -7,6 +7,13 @@ class Password extends FormzInput<String, PasswordError> {
 
   const Password.dirty(super.value) : super.dirty();
 
+  String? get errorMessage {
+    if (isValid || isPure) return null;
+    if (displayError == PasswordError.empty) return 'El campo es requerido';
+    if (displayError == PasswordError.length) return 'Mínimo 6 caracteres';
+    return null;
+  }
+
   @override
   PasswordError? validator(String value) {
     if (value.isEmpty || value.trim().isEmpty) return PasswordError.empty;
--- Flutter/forms_app/lib/infrastructure/inputs/username.dart
+++ Flutter/forms_app/lib/infrastructure/inputs/username.dart
@@ -7,6 +7,13 @@ class Username extends FormzInput<String, UsernameError> {
 
   const Username.dirty(super.value) : super.dirty();
 
+  String? get errorMessage {
+    if(isValid || isPure) return null;
+    if(displayError == UsernameError.empty) return 'El campo es requerido';
+    if(displayError == UsernameError.length) return 'Mínimo 6 caracteres';
+    return null;
+  }
+
   @override
   UsernameError? validator(String value) {
     if (value.isEmpty || value.trim().isEmpty) return UsernameError.empty;
