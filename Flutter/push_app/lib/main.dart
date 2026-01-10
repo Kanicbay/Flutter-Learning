ee9ed3a01dea6b84f480e681664db0bcb0e12d40
Brian Coyago <briancoyago@hotmail.com>
2026-01-10 18:51:06 -0500

    feat(push_app): init firebase instance
    Brian Coyago

--- Flutter/push_app/lib/main.dart
+++ Flutter/push_app/lib/main.dart
@@ -5,8 +5,15 @@ import 'package:push_app/config/router/app_router.dart';
 import 'package:push_app/config/theme/app_theme.dart';
 import 'package:push_app/presentation/blocs/notifications/notifications_bloc.dart';
 
-void main() {
-  runApp(MultiBlocProvider(providers: [BlocProvider(create: (_) => NotificationsBloc())], child: MainApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NotificationsBloc.initializerFirebaseNotifications();
+  runApp(
+    MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => NotificationsBloc())],
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
