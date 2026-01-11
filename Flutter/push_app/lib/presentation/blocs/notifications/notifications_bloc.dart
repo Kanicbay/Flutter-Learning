c3e2e83619bf02ed8ff84bd6eebcdefb20f6f1eb
Brian Coyago <briancoyago@hotmail.com>
2026-01-10 20:09:50 -0500

    feat(push_app): create push message entity
    Brian Coyago

--- Flutter/push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
+++ Flutter/push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:push_app/domain/entities/push_message.dart';
 import 'package:push_app/firebase_options.dart';
 
 part 'notifications_event.dart';
@@ -47,11 +50,19 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   }
 
   void _handleRemoteMessage(RemoteMessage message) async {
-    print('Got a message whilst in the foreground!');
-    print('Message data: ${message.data}');
-
     if (message.notification == null) return null;
-    print('Message also contained a notification: ${message.notification}');
+    final notification = PushMessage(
+      messageId:
+          message.messageId?.replaceAll(':', '').replaceAll('%', '') ?? '',
+      title: message.notification?.title ?? '',
+      body: message.notification?.body ?? '',
+      sendDate: message.sentTime ?? DateTime.now(),
+      data: message.data,
+      imageUrl: Platform.isAndroid
+          ? message.notification!.android?.imageUrl
+          : message.notification!.apple?.imageUrl,
+    );
+    print(notification);
   }
 
   void _onForeGroundMessage() async {
