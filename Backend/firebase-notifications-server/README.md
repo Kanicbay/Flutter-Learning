768d4b728f0ed1cdeb7311ebdb2e2f5e4fb9d773
Brian Coyago <briancoyago@hotmail.com>
2026-01-11 19:55:48 -0500

    fix(push_app): change main folder
    Brian Coyago

--- Backend/firebase-notifications-server/README.md
+++ Backend/firebase-notifications-server/README.md
@@ -1,3 +1,18 @@
 # Simple Firebase Notification Server
 
 This server is made for flutter project
+
+[Firebase Official Documentation](https://firebase.google.com/docs/cloud-messaging/auth-server)
+
+## Pasos
+
+1. Install Dependecies
+```
+npm install
+```
+
+2. Get configuration Firebase file (firebase-admin.json)
+3. Init server
+```
+node app
+```
--- Backend/firebase-notifications-server/app.js
+++ Backend/firebase-notifications-server/app.js
@@ -1,8 +1,8 @@
-import express from 'express';
-const {google} = require('googleapis');
+const express = require('express');
+const { google } = require('googleapis');
 
-const admin = require('firebase-admin');
-const serviceAccount = require('././firebase-admin.json');
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase-admin.json");
 
 const app = express();
 const port = 3000;
@@ -10,13 +10,14 @@ const port = 3000;
 const MESSAGING_SCOPE = 'https://www.googleapis.com/auth/firebase.messaging';
 const SCOPES = [MESSAGING_SCOPE];
 
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 function getAccessToken() {
     return new Promise(function(resolve, reject) {
-      const key = require('././firebase-admin.json');
+      const key = require('./firebase-admin.json');
       const jwtClient = new google.auth.JWT(
         key.client_email,
         null,
@@ -36,8 +37,12 @@ function getAccessToken() {
 
 app.get('/', async(req, res) => {
     const token = await getAccessToken()
+
     res.json(token);
 });
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
