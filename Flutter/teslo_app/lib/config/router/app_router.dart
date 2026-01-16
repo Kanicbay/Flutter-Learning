9dc3bd2918ec31e769bd9084ed687069500f9340
Brian Coyago <briancoyago@hotmail.com>
2026-01-15 19:13:25 -0500

    feat(teslo_app): created inputs for login form
    Brian Coyago

--- Flutter/teslo_app/lib/config/router/app_router.dart
+++ Flutter/teslo_app/lib/config/router/app_router.dart
@@ -3,23 +3,16 @@ import 'package:teslo_app/features/auth/auth.dart';
 import 'package:teslo_app/features/products/products.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/login',
   routes: [
-
     ///* Auth Routes
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
+    GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
     GoRoute(
       path: '/register',
       builder: (context, state) => const RegisterScreen(),
     ),
 
     ///* Product Routes
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
+    GoRoute(path: '/', builder: (context, state) => const ProductsScreen()),
   ],
 );
