577bc6cbf84efa669858198a37a70b8df4ce0c9b
Brian Coyago <briancoyago@hotmail.com>
2026-01-15 22:29:43 -0500

    feat(teslo-app): implements login datasource
    Brian Coyago

--- Flutter/teslo_app/lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ Flutter/teslo_app/lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -1,20 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_app/config/config.dart';
 import 'package:teslo_app/features/auth/domain/domain.dart';
+import 'package:teslo_app/features/auth/infrastructure/infrastructure.dart';
 
-class AuthDatasourceImpl extends AuthDatasource{
+class AuthDatasourceImpl extends AuthDatasource {
+  final dio = Dio(BaseOptions(baseUrl: Environment.apiUrl));
+
   @override
-  Future<User> checkAuthStatus(String token) {
+  Future<User> checkAuthStatus(String token) async {
     throw UnimplementedError();
   }
 
   @override
-  Future<User> login(String email, String password) {
-    throw UnimplementedError();
+  Future<User> login(String email, String password) async {
+    try {
+      final response = await dio.post(
+        '/auth/login',
+        data: {'email': email, 'password': password},
+      );
+
+      final user = UserMapper.userJsonToEntity(response.data);
+      return user;
+    } catch (e) {
+      throw WrongCredentials();
+    }
   }
 
   @override
-  Future<User> register(String email, String password, String fullName) {
+  Future<User> register(String email, String password, String fullName) async {
     throw UnimplementedError();
   }
-
-
 }
