a239dd217efc3629a2c8b347b75aa53864cb731d
Brian Coyago <briancoyago@hotmail.com>
2026-01-15 22:20:43 -0500

    feat(teslo-app): create domain and infrastructure skeleton
    Brian Coyago

---
+++ Flutter/teslo_app/lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -0,0 +1,20 @@
+import 'package:teslo_app/features/auth/domain/domain.dart';
+
+class AuthDatasourceImpl extends AuthDatasource{
+  @override
+  Future<User> checkAuthStatus(String token) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<User> login(String email, String password) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<User> register(String email, String password, String fullName) {
+    throw UnimplementedError();
+  }
+
+
+}
