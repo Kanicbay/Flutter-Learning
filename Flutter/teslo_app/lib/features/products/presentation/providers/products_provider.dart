16bed8b5bed98183cc9455675df0d69e968f7e95
Brian Coyago <briancoyago@hotmail.com>
2026-01-17 17:48:45 -0500

    feat(teslo-app): create products state
    Brian Coyago

---
+++ Flutter/teslo_app/lib/features/products/presentation/providers/products_provider.dart
@@ -0,0 +1,31 @@
+import 'package:teslo_app/features/products/domain/domain.dart';
+
+class ProductState {
+  final bool isLastPage;
+  final int limit;
+  final int offset;
+  final bool isLoading;
+  final List<Product> products;
+
+  ProductState({
+    this.isLastPage = false,
+    this.limit = 10,
+    this.offset = 0,
+    this.isLoading = false,
+    this.products = const [],
+  });
+
+  ProductState copyWith({
+    bool? isLastPage,
+    int? limit,
+    int? offset,
+    bool? isLoading,
+    List<Product>? products,
+  }) => ProductState(
+    isLastPage: isLastPage ?? this.isLastPage,
+    limit: limit ?? this.limit,
+    offset: offset ?? this.offset,
+    isLoading: isLoading ?? this.isLoading,
+    products: products ?? this.products,
+  );
+}
