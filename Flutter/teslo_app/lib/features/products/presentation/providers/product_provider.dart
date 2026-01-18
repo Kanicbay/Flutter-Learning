8d3e988f3a68afaa04ce72f3d10ba095be201a8c
Brian Coyago <briancoyago@hotmail.com>
2026-01-18 14:30:17 -0500

    feat(teslo-app): create product provider
    Brian Coyago

---
+++ Flutter/teslo_app/lib/features/products/presentation/providers/product_provider.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_app/features/products/domain/domain.dart';
+import 'package:teslo_app/features/products/presentation/providers/providers.dart';
+
+class ProductState {
+  final String id;
+  final Product? product;
+  final bool isLoading;
+  final bool isSaving;
+
+  ProductState({
+    required this.id,
+    this.product,
+    this.isLoading = true,
+    this.isSaving = false,
+  });
+
+  ProductState copyWith({
+    String? id,
+    Product? product,
+    bool? isLoading,
+    bool? isSaving,
+  }) => ProductState(
+    id: id ?? this.id,
+    product: product ?? this.product,
+    isLoading: isLoading ?? this.isLoading,
+    isSaving: isSaving ?? this.isSaving,
+  );
+}
+
+class ProductNotifier extends Notifier<ProductState> {
+  ProductsRepository get productsRepository =>
+      ref.watch(productsRepositoryProvider);
+
+  final String id;
+  ProductNotifier(this.id);
+
+  @override
+  ProductState build() => ProductState(id: id);
+
+  Future<void> loadProduct() async {
+    
+  }
+}
+
+final productProvider = NotifierProvider.autoDispose
+    .family<ProductNotifier, ProductState, String>((id) => ProductNotifier(id));
