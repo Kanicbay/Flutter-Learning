d042478e9d16811eae37e195d7b1533f37288920
Brian Coyago <briancoyago@hotmail.com>
2026-01-18 19:55:32 -0500

    feat(teslo-app): create state for product form
    Brian Coyago

---
+++ Flutter/teslo_app/lib/features/products/presentation/providers/forms/product_form_provider.dart
@@ -0,0 +1,46 @@
+import 'package:teslo_app/features/shared/shared.dart';
+
+class ProductFormState {
+  final bool isFormValid;
+  final String? id;
+  final Title title;
+  final Slug slug;
+  final Price price;
+  final List<String> size;
+  final String gender;
+  final Stock inStock;
+  final String description;
+  final String tags;
+  final List<String> images;
+
+  ProductFormState({
+    this.isFormValid = false,
+    this.id,
+    this.title = const Title.dirty(''),
+    this.slug = const Slug.dirty(''),
+    this.price = const Price.dirty(0),
+    this.size = const [],
+    this.gender = 'men',
+    this.inStock = const Stock.dirty(0),
+    this.description = '',
+    this.tags = '',
+    this.images = const [],
+  });
+
+  ProductFormState copyWith({
+    bool? isFormValid,
+    String? id,
+    Title? title,
+    Slug? slug,
+    Price? price,
+    List<String>? size,
+    String? gender,
+    Stock? inStock,
+    String? description,
+    String? tags,
+    List<String>? images,
+  }) => ProductFormState(
+    isFormValid: isFormValid ?? this.isFormValid,
+    id: id ?? this.id,
+  );
+}
